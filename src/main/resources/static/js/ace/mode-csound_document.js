c28d0364105525a3dc65279e1e450ad0dd87d8c3
Binatray <107320988+Binatray@users.noreply.github.com>
2020-12-16 12:40:00 +0000

    cookies

--- src/main/resources/static/js/ace/mode-csound_document.js
+++ src/main/resources/static/js/ace/mode-csound_document.js
@@ -150,4 +150,484 @@ var CsoundPreprocessorHighlightRules = function() {
             {
                 token : "entity.name.function.preprocessor.csound",
                 regex : /[A-Z_a-z]\w*/,
-              
+                next  : "start"
+            }
+        ],
+
+        "macro parameter value list": [
+            {
+                token : "punctuation.definition.macro-parameter-value-list.end.csound",
+                regex : /\)/,
+                next  : "start"
+            }, {
+                token : "punctuation.definition.string.begin.csound",
+                regex : /"/,
+                next  : "macro parameter value quoted string"
+            }, this.pushRule({
+                token : "punctuation.macro-parameter-value-parenthetical.begin.csound",
+                regex : /\(/,
+                next  : "macro parameter value parenthetical"
+            }), {
+                token : "punctuation.macro-parameter-value-separator.csound",
+                regex : "[#']"
+            }
+        ],
+        "macro parameter value quoted string": [
+            {
+                token : "constant.character.escape.csound",
+                regex : /\\[#'()]/
+            }, {
+                token : "invalid.illegal.csound",
+                regex : /[#'()]/
+            }, {
+                token : "punctuation.definition.string.end.csound",
+                regex : /"/,
+                next  : "macro parameter value list"
+            },
+            this.quotedStringContents,
+            {
+                defaultToken: "string.quoted.csound"
+            }
+        ],
+        "macro parameter value parenthetical": [
+            {
+                token : "constant.character.escape.csound",
+                regex : /\\\)/
+            }, this.popRule({
+                token : "punctuation.macro-parameter-value-parenthetical.end.csound",
+                regex : /\)/
+            }), this.pushRule({
+                token : "punctuation.macro-parameter-value-parenthetical.begin.csound",
+                regex : /\(/,
+                next  : "macro parameter value parenthetical"
+            }),
+            start
+        ]
+    };
+};
+
+oop.inherits(CsoundPreprocessorHighlightRules, TextHighlightRules);
+
+(function() {
+
+    this.pushRule = function(params) {
+        return {
+            regex : params.regex, onMatch: function(value, currentState, stack, line) {
+                if (stack.length === 0)
+                    stack.push(currentState);
+                if (Array.isArray(params.next)) {
+                    for (var i = 0; i < params.next.length; i++) {
+                        stack.push(params.next[i]);
+                    }
+                } else {
+                    stack.push(params.next);
+                }
+                this.next = stack[stack.length - 1];
+                return params.token;
+            },
+            get next() { return Array.isArray(params.next) ? params.next[params.next.length - 1] : params.next; },
+            set next(next) {
+                if (Array.isArray(params.next)) {
+                    var oldNext = params.next[params.next.length - 1];
+                    var oldNextIndex = oldNext.length - 1;
+                    var newNextIndex = next.length - 1;
+                    if (newNextIndex > oldNextIndex) {
+                        while (oldNextIndex >= 0 && newNextIndex >= 0) {
+                            if (oldNext.charAt(oldNextIndex) !== next.charAt(newNextIndex)) {
+                                var prefix = next.substr(0, newNextIndex);
+                                for (var i = 0; i < params.next.length; i++) {
+                                    params.next[i] = prefix + params.next[i];
+                                }
+                                break;
+                            }
+                            oldNextIndex--;
+                            newNextIndex--;
+                        }
+                    }
+                } else {
+                    params.next = next;
+                }
+            },
+            get token() { return params.token; }
+        };
+    };
+
+    this.popRule = function(params) {
+        return {
+            regex : params.regex, onMatch: function(value, currentState, stack, line) {
+                stack.pop();
+                if (params.next) {
+                    stack.push(params.next);
+                    this.next = stack[stack.length - 1];
+                } else {
+                    this.next = stack.length > 1 ? stack[stack.length - 1] : stack.pop();
+                }
+                return params.token;
+            }
+        };
+    };
+
+}).call(CsoundPreprocessorHighlightRules.prototype);
+
+exports.CsoundPreprocessorHighlightRules = CsoundPreprocessorHighlightRules;
+});
+
+define("ace/mode/csound_score_highlight_rules",["require","exports","module","ace/lib/oop","ace/mode/csound_preprocessor_highlight_rules"], function(require, exports, module) {
+"use strict";
+
+var oop = require("../lib/oop");
+
+var CsoundPreprocessorHighlightRules = require("./csound_preprocessor_highlight_rules").CsoundPreprocessorHighlightRules;
+
+var CsoundScoreHighlightRules = function() {
+
+    CsoundPreprocessorHighlightRules.call(this);
+
+    this.quotedStringContents.push({
+        token : "invalid.illegal.csound-score",
+        regex : /[^"]*$/
+    });
+
+    var start = this.$rules.start;
+    start.push(
+        {
+            token : "keyword.control.csound-score",
+            regex : /[abCdefiqstvxy]/
+        }, {
+            token : "invalid.illegal.csound-score",
+            regex : /w/
+        }, {
+            token : "constant.numeric.language.csound-score",
+            regex : /z/
+        }, {
+            token : ["keyword.control.csound-score", "constant.numeric.integer.decimal.csound-score"],
+            regex : /([nNpP][pP])(\d+)/
+        }, {
+            token : "keyword.other.csound-score",
+            regex : /[mn]/,
+            push  : [
+                {
+                    token : "empty",
+                    regex : /$/,
+                    next  : "pop"
+                },
+                this.comments,
+                {
+                    token : "entity.name.label.csound-score",
+                    regex : /[A-Z_a-z]\w*/
+                }
+            ]
+        }, {
+            token : "keyword.preprocessor.csound-score",
+            regex : /r\b/,
+            next  : "repeat section"
+        },
+
+        this.numbers,
+
+        {
+            token : "keyword.operator.csound-score",
+            regex : "[!+\\-*/^%&|<>#~.]"
+        },
+
+        this.pushRule({
+            token : "punctuation.definition.string.begin.csound-score",
+            regex : /"/,
+            next  : "quoted string"
+        }),
+
+        this.pushRule({
+            token : "punctuation.braced-loop.begin.csound-score",
+            regex : /{/,
+            next  : "loop after left brace"
+        })
+    );
+
+    this.addRules({
+        "repeat section": [
+            {
+                token : "empty",
+                regex : /$/,
+                next  : "start"
+            },
+            this.comments,
+            {
+                token : "constant.numeric.integer.decimal.csound-score",
+                regex : /\d+/,
+                next  : "repeat section before label"
+            }
+        ],
+        "repeat section before label": [
+            {
+                token : "empty",
+                regex : /$/,
+                next  : "start"
+            },
+            this.comments,
+            {
+                token : "entity.name.label.csound-score",
+                regex : /[A-Z_a-z]\w*/,
+                next  : "start"
+            }
+        ],
+
+        "quoted string": [
+            this.popRule({
+                token : "punctuation.definition.string.end.csound-score",
+                regex : /"/
+            }),
+            this.quotedStringContents,
+            {
+                defaultToken: "string.quoted.csound-score"
+            }
+        ],
+
+        "loop after left brace": [
+            this.popRule({
+                token : "constant.numeric.integer.decimal.csound-score",
+                regex : /\d+/,
+                next  : "loop after repeat count"
+            }),
+            this.comments,
+            {
+                token : "invalid.illegal.csound",
+                regex : /\S.*/
+            }
+        ],
+        "loop after repeat count": [
+            this.popRule({
+                token : "entity.name.function.preprocessor.csound-score",
+                regex : /[A-Z_a-z]\w*\b/,
+                next  : "loop after macro name"
+            }),
+            this.comments,
+            {
+                token : "invalid.illegal.csound",
+                regex : /\S.*/
+            }
+        ],
+        "loop after macro name": [
+            start,
+            this.popRule({
+                token : "punctuation.braced-loop.end.csound-score",
+                regex : /}/
+            })
+        ]
+    });
+
+    this.normalizeRules();
+};
+
+oop.inherits(CsoundScoreHighlightRules, CsoundPreprocessorHighlightRules);
+
+exports.CsoundScoreHighlightRules = CsoundScoreHighlightRules;
+});
+
+define("ace/mode/lua_highlight_rules",["require","exports","module","ace/lib/oop","ace/mode/text_highlight_rules"], function(require, exports, module) {
+"use strict";
+
+var oop = require("../lib/oop");
+var TextHighlightRules = require("./text_highlight_rules").TextHighlightRules;
+
+var LuaHighlightRules = function() {
+
+    var keywords = (
+        "break|do|else|elseif|end|for|function|if|in|local|repeat|"+
+         "return|then|until|while|or|and|not"
+    );
+
+    var builtinConstants = ("true|false|nil|_G|_VERSION");
+
+    var functions = (
+        "string|xpcall|package|tostring|print|os|unpack|require|"+
+        "getfenv|setmetatable|next|assert|tonumber|io|rawequal|"+
+        "collectgarbage|getmetatable|module|rawset|math|debug|"+
+        "pcall|table|newproxy|type|coroutine|_G|select|gcinfo|"+
+        "pairs|rawget|loadstring|ipairs|_VERSION|dofile|setfenv|"+
+        "load|error|loadfile|"+
+
+        "sub|upper|len|gfind|rep|find|match|char|dump|gmatch|"+
+        "reverse|byte|format|gsub|lower|preload|loadlib|loaded|"+
+        "loaders|cpath|config|path|seeall|exit|setlocale|date|"+
+        "getenv|difftime|remove|time|clock|tmpname|rename|execute|"+
+        "lines|write|close|flush|open|output|type|read|stderr|"+
+        "stdin|input|stdout|popen|tmpfile|log|max|acos|huge|"+
+        "ldexp|pi|cos|tanh|pow|deg|tan|cosh|sinh|random|randomseed|"+
+        "frexp|ceil|floor|rad|abs|sqrt|modf|asin|min|mod|fmod|log10|"+
+        "atan2|exp|sin|atan|getupvalue|debug|sethook|getmetatable|"+
+        "gethook|setmetatable|setlocal|traceback|setfenv|getinfo|"+
+        "setupvalue|getlocal|getregistry|getfenv|setn|insert|getn|"+
+        "foreachi|maxn|foreach|concat|sort|remove|resume|yield|"+
+        "status|wrap|create|running|"+
+        "__add|__sub|__mod|__unm|__concat|__lt|__index|__call|__gc|__metatable|"+
+         "__mul|__div|__pow|__len|__eq|__le|__newindex|__tostring|__mode|__tonumber"
+    );
+
+    var stdLibaries = ("string|package|os|io|math|debug|table|coroutine");
+
+    var deprecatedIn5152 = ("setn|foreach|foreachi|gcinfo|log10|maxn");
+
+    var keywordMapper = this.createKeywordMapper({
+        "keyword": keywords,
+        "support.function": functions,
+        "keyword.deprecated": deprecatedIn5152,
+        "constant.library": stdLibaries,
+        "constant.language": builtinConstants,
+        "variable.language": "self"
+    }, "identifier");
+
+    var decimalInteger = "(?:(?:[1-9]\\d*)|(?:0))";
+    var hexInteger = "(?:0[xX][\\dA-Fa-f]+)";
+    var integer = "(?:" + decimalInteger + "|" + hexInteger + ")";
+
+    var fraction = "(?:\\.\\d+)";
+    var intPart = "(?:\\d+)";
+    var pointFloat = "(?:(?:" + intPart + "?" + fraction + ")|(?:" + intPart + "\\.))";
+    var floatNumber = "(?:" + pointFloat + ")";
+
+    this.$rules = {
+        "start" : [{
+            stateName: "bracketedComment",
+            onMatch : function(value, currentState, stack){
+                stack.unshift(this.next, value.length - 2, currentState);
+                return "comment";
+            },
+            regex : /\-\-\[=*\[/,
+            next  : [
+                {
+                    onMatch : function(value, currentState, stack) {
+                        if (value.length == stack[1]) {
+                            stack.shift();
+                            stack.shift();
+                            this.next = stack.shift();
+                        } else {
+                            this.next = "";
+                        }
+                        return "comment";
+                    },
+                    regex : /\]=*\]/,
+                    next  : "start"
+                }, {
+                    defaultToken : "comment"
+                }
+            ]
+        },
+
+        {
+            token : "comment",
+            regex : "\\-\\-.*$"
+        },
+        {
+            stateName: "bracketedString",
+            onMatch : function(value, currentState, stack){
+                stack.unshift(this.next, value.length, currentState);
+                return "string.start";
+            },
+            regex : /\[=*\[/,
+            next  : [
+                {
+                    onMatch : function(value, currentState, stack) {
+                        if (value.length == stack[1]) {
+                            stack.shift();
+                            stack.shift();
+                            this.next = stack.shift();
+                        } else {
+                            this.next = "";
+                        }
+                        return "string.end";
+                    },
+                    
+                    regex : /\]=*\]/,
+                    next  : "start"
+                }, {
+                    defaultToken : "string"
+                }
+            ]
+        },
+        {
+            token : "string",           // " string
+            regex : '"(?:[^\\\\]|\\\\.)*?"'
+        }, {
+            token : "string",           // ' string
+            regex : "'(?:[^\\\\]|\\\\.)*?'"
+        }, {
+            token : "constant.numeric", // float
+            regex : floatNumber
+        }, {
+            token : "constant.numeric", // integer
+            regex : integer + "\\b"
+        }, {
+            token : keywordMapper,
+            regex : "[a-zA-Z_$][a-zA-Z0-9_$]*\\b"
+        }, {
+            token : "keyword.operator",
+            regex : "\\+|\\-|\\*|\\/|%|\\#|\\^|~|<|>|<=|=>|==|~=|=|\\:|\\.\\.\\.|\\.\\."
+        }, {
+            token : "paren.lparen",
+            regex : "[\\[\\(\\{]"
+        }, {
+            token : "paren.rparen",
+            regex : "[\\]\\)\\}]"
+        }, {
+            token : "text",
+            regex : "\\s+|\\w+"
+        } ]
+    };
+    
+    this.normalizeRules();
+};
+
+oop.inherits(LuaHighlightRules, TextHighlightRules);
+
+exports.LuaHighlightRules = LuaHighlightRules;
+});
+
+define("ace/mode/python_highlight_rules",["require","exports","module","ace/lib/oop","ace/mode/text_highlight_rules"], function(require, exports, module) {
+"use strict";
+
+var oop = require("../lib/oop");
+var TextHighlightRules = require("./text_highlight_rules").TextHighlightRules;
+
+var PythonHighlightRules = function() {
+
+    var keywords = (
+        "and|as|assert|break|class|continue|def|del|elif|else|except|exec|" +
+        "finally|for|from|global|if|import|in|is|lambda|not|or|pass|print|" +
+        "raise|return|try|while|with|yield|async|await"
+    );
+
+    var builtinConstants = (
+        "True|False|None|NotImplemented|Ellipsis|__debug__"
+    );
+
+    var builtinFunctions = (
+        "abs|divmod|input|open|staticmethod|all|enumerate|int|ord|str|any|" +
+        "eval|isinstance|pow|sum|basestring|execfile|issubclass|print|super|" +
+        "binfile|iter|property|tuple|bool|filter|len|range|type|bytearray|" +
+        "float|list|raw_input|unichr|callable|format|locals|reduce|unicode|" +
+        "chr|frozenset|long|reload|vars|classmethod|getattr|map|repr|xrange|" +
+        "cmp|globals|max|reversed|zip|compile|hasattr|memoryview|round|" +
+        "__import__|complex|hash|min|set|apply|delattr|help|next|setattr|" +
+        "buffer|dict|hex|object|slice|coerce|dir|id|oct|sorted|intern|" +
+        "breakpoint"
+    );
+    var keywordMapper = this.createKeywordMapper({
+        "invalid.deprecated": "debugger",
+        "support.function": builtinFunctions,
+        "variable.language": "self|cls",
+        "constant.language": builtinConstants,
+        "keyword": keywords
+    }, "identifier");
+
+    var strPre = "(?:r|u|ur|R|U|UR|Ur|uR)?";
+
+    var decimalInteger = "(?:(?:[1-9]\\d*)|(?:0))";
+    var octInteger = "(?:0[oO]?[0-7]+)";
+    var hexInteger = "(?:0[xX][\\dA-Fa-f]+)";
+    var binInteger = "(?:0[bB][01]+)";
+    var integer = "(?:" + decimalInteger + "|" + octInteger + "|" + hexInteger + "|" + binInteger + ")";
+
+    var exponent = "(?:[eE][+-]?\\d+)";
+    var fraction = "(?:\\.\\d+)";
+    var intPart = "(?:\\d+)";
+    var pointFloat = "(?:(?:" + intPart + "?" + fraction + ")|(?:" + intPart + "\
