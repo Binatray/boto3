41fc4653e2b4f0942c658e9eba315c4218c3b0da
Binatray <107320988+Binatray@users.noreply.github.com>
2021-03-25 10:47:48 +0000

    Did ANOTHER thing

--- src/main/resources/static/js/ace/mode-php.js
+++ src/main/resources/static/js/ace/mode-php.js
@@ -5993,4 +5993,409 @@ var functionMap = {
         "Modify the name of an entry"
     ],
     "ldap_sasl_bind": [
-        "bool ldap_sasl_bind(resource link [, string binddn [, string password [, string sasl_mech [, string sasl_realm [, string sasl_authc_id [, string sasl_authz_id [, s
+        "bool ldap_sasl_bind(resource link [, string binddn [, string password [, string sasl_mech [, string sasl_realm [, string sasl_authc_id [, string sasl_authz_id [, string props]]]]]]])",
+        "Bind to LDAP directory using SASL"
+    ],
+    "ldap_search": [
+        "resource ldap_search(resource|array link, string base_dn, string filter [, array attrs [, int attrsonly [, int sizelimit [, int timelimit [, int deref]]]]])",
+        "Search LDAP tree under base_dn"
+    ],
+    "ldap_set_option": [
+        "bool ldap_set_option(resource link, int option, mixed newval)",
+        "Set the value of various session-wide parameters"
+    ],
+    "ldap_set_rebind_proc": [
+        "bool ldap_set_rebind_proc(resource link, string callback)",
+        "Set a callback function to do re-binds on referral chasing."
+    ],
+    "ldap_sort": [
+        "bool ldap_sort(resource link, resource result, string sortfilter)",
+        "Sort LDAP result entries"
+    ],
+    "ldap_start_tls": [
+        "bool ldap_start_tls(resource link)",
+        "Start TLS"
+    ],
+    "ldap_t61_to_8859": [
+        "string ldap_t61_to_8859(string value)",
+        "Translate t61 characters to 8859 characters"
+    ],
+    "ldap_unbind": [
+        "bool ldap_unbind(resource link)",
+        "Unbind from LDAP directory"
+    ],
+    "leak": [
+        "void leak(int num_bytes=3)",
+        "Cause an intentional memory leak, for testing/debugging purposes"
+    ],
+    "levenshtein": [
+        "int levenshtein(string str1, string str2[, int cost_ins, int cost_rep, int cost_del])",
+        "Calculate Levenshtein distance between two strings"
+    ],
+    "libxml_clear_errors": [
+        "void libxml_clear_errors()",
+        "Clear last error from libxml"
+    ],
+    "libxml_disable_entity_loader": [
+        "bool libxml_disable_entity_loader([boolean disable])",
+        "Disable/Enable ability to load external entities"
+    ],
+    "libxml_get_errors": [
+        "object libxml_get_errors()",
+        "Retrieve array of errors"
+    ],
+    "libxml_get_last_error": [
+        "object libxml_get_last_error()",
+        "Retrieve last error from libxml"
+    ],
+    "libxml_set_streams_context": [
+        "void libxml_set_streams_context(resource streams_context)",
+        "Set the streams context for the next libxml document load or write"
+    ],
+    "libxml_use_internal_errors": [
+        "bool libxml_use_internal_errors([boolean use_errors])",
+        "Disable libxml errors and allow user to fetch error information as needed"
+    ],
+    "link": [
+        "int link(string target, string link)",
+        "Create a hard link"
+    ],
+    "linkinfo": [
+        "int linkinfo(string filename)",
+        "Returns the st_dev field of the UNIX C stat structure describing the link"
+    ],
+    "litespeed_request_headers": [
+        "array litespeed_request_headers(void)",
+        "Fetch all HTTP request headers"
+    ],
+    "litespeed_response_headers": [
+        "array litespeed_response_headers(void)",
+        "Fetch all HTTP response headers"
+    ],
+    "locale_accept_from_http": [
+        "string locale_accept_from_http(string $http_accept)",
+        null
+    ],
+    "locale_canonicalize": [
+        "static string locale_canonicalize(Locale $loc, string $locale)",
+        "* @param string $locale The locale string to canonicalize"
+    ],
+    "locale_filter_matches": [
+        "boolean locale_filter_matches(string $langtag, string $locale[, bool $canonicalize])",
+        "* Checks if a $langtag filter matches with $locale according to RFC 4647's basic filtering algorithm"
+    ],
+    "locale_get_all_variants": [
+        "static array locale_get_all_variants($locale)",
+        "* gets an array containing the list of variants, or null"
+    ],
+    "locale_get_default": [
+        "static string locale_get_default( )",
+        "Get default locale"
+    ],
+    "locale_get_keywords": [
+        "static array locale_get_keywords(string $locale) {",
+        "* return an associative array containing keyword-value  * pairs for this locale. The keys are keys to the array (doh!)"
+    ],
+    "locale_get_primary_language": [
+        "static string locale_get_primary_language($locale)",
+        "* gets the primary language for the $locale"
+    ],
+    "locale_get_region": [
+        "static string locale_get_region($locale)",
+        "* gets the region for the $locale"
+    ],
+    "locale_get_script": [
+        "static string locale_get_script($locale)",
+        "* gets the script for the $locale"
+    ],
+    "locale_lookup": [
+        "string locale_lookup(array $langtag, string $locale[, bool $canonicalize[, string $default = null]])",
+        "* Searchs the items in $langtag for the best match to the language * range"
+    ],
+    "locale_set_default": [
+        "static string locale_set_default( string $locale )",
+        "Set default locale"
+    ],
+    "localeconv": [
+        "array localeconv(void)",
+        "Returns numeric formatting information based on the current locale"
+    ],
+    "localtime": [
+        "array localtime([int timestamp [, bool associative_array]])",
+        "Returns the results of the C system call localtime as an associative array if the associative_array argument is set to 1 other wise it is a regular array"
+    ],
+    "log": [
+        "float log(float number, [float base])",
+        "Returns the natural logarithm of the number, or the base log if base is specified"
+    ],
+    "log10": [
+        "float log10(float number)",
+        "Returns the base-10 logarithm of the number"
+    ],
+    "log1p": [
+        "float log1p(float number)",
+        "Returns log(1 + number), computed in a way that accurate even when the value of number is close to zero"
+    ],
+    "long2ip": [
+        "string long2ip(int proper_address)",
+        "Converts an (IPv4) Internet network address into a string in Internet standard dotted format"
+    ],
+    "lstat": [
+        "array lstat(string filename)",
+        "Give information about a file or symbolic link"
+    ],
+    "ltrim": [
+        "string ltrim(string str [, string character_mask])",
+        "Strips whitespace from the beginning of a string"
+    ],
+    "mail": [
+        "int mail(string to, string subject, string message [, string additional_headers [, string additional_parameters]])",
+        "Send an email message"
+    ],
+    "max": [
+        "mixed max(mixed arg1 [, mixed arg2 [, mixed ...]])",
+        "Return the highest value in an array or a series of arguments"
+    ],
+    "mb_check_encoding": [
+        "bool mb_check_encoding([string var[, string encoding]])",
+        "Check if the string is valid for the specified encoding"
+    ],
+    "mb_convert_case": [
+        "string mb_convert_case(string sourcestring, int mode [, string encoding])",
+        "Returns a case-folded version of sourcestring"
+    ],
+    "mb_convert_encoding": [
+        "string mb_convert_encoding(string str, string to-encoding [, mixed from-encoding])",
+        "Returns converted string in desired encoding"
+    ],
+    "mb_convert_kana": [
+        "string mb_convert_kana(string str [, string option] [, string encoding])",
+        "Conversion between full-width character and half-width character (Japanese)"
+    ],
+    "mb_convert_variables": [
+        "string mb_convert_variables(string to-encoding, mixed from-encoding, mixed vars [, ...])",
+        "Converts the string resource in variables to desired encoding"
+    ],
+    "mb_decode_mimeheader": [
+        "string mb_decode_mimeheader(string string)",
+        "Decodes the MIME \"encoded-word\" in the string"
+    ],
+    "mb_decode_numericentity": [
+        "string mb_decode_numericentity(string string, array convmap [, string encoding])",
+        "Converts HTML numeric entities to character code"
+    ],
+    "mb_detect_encoding": [
+        "string mb_detect_encoding(string str [, mixed encoding_list [, bool strict]])",
+        "Encodings of the given string is returned (as a string)"
+    ],
+    "mb_detect_order": [
+        "bool|array mb_detect_order([mixed encoding-list])",
+        "Sets the current detect_order or Return the current detect_order as a array"
+    ],
+    "mb_encode_mimeheader": [
+        "string mb_encode_mimeheader(string str [, string charset [, string transfer-encoding [, string linefeed [, int indent]]]])",
+        "Converts the string to MIME \"encoded-word\" in the format of =?charset?(B|Q)?encoded_string?="
+    ],
+    "mb_encode_numericentity": [
+        "string mb_encode_numericentity(string string, array convmap [, string encoding])",
+        "Converts specified characters to HTML numeric entities"
+    ],
+    "mb_encoding_aliases": [
+        "array mb_encoding_aliases(string encoding)",
+        "Returns an array of the aliases of a given encoding name"
+    ],
+    "mb_ereg": [
+        "int mb_ereg(string pattern, string string [, array registers])",
+        "Regular expression match for multibyte string"
+    ],
+    "mb_ereg_match": [
+        "bool mb_ereg_match(string pattern, string string [,string option])",
+        "Regular expression match for multibyte string"
+    ],
+    "mb_ereg_replace": [
+        "string mb_ereg_replace(string pattern, string replacement, string string [, string option])",
+        "Replace regular expression for multibyte string"
+    ],
+    "mb_ereg_search": [
+        "bool mb_ereg_search([string pattern[, string option]])",
+        "Regular expression search for multibyte string"
+    ],
+    "mb_ereg_search_getpos": [
+        "int mb_ereg_search_getpos(void)",
+        "Get search start position"
+    ],
+    "mb_ereg_search_getregs": [
+        "array mb_ereg_search_getregs(void)",
+        "Get matched substring of the last time"
+    ],
+    "mb_ereg_search_init": [
+        "bool mb_ereg_search_init(string string [, string pattern[, string option]])",
+        "Initialize string and regular expression for search."
+    ],
+    "mb_ereg_search_pos": [
+        "array mb_ereg_search_pos([string pattern[, string option]])",
+        "Regular expression search for multibyte string"
+    ],
+    "mb_ereg_search_regs": [
+        "array mb_ereg_search_regs([string pattern[, string option]])",
+        "Regular expression search for multibyte string"
+    ],
+    "mb_ereg_search_setpos": [
+        "bool mb_ereg_search_setpos(int position)",
+        "Set search start position"
+    ],
+    "mb_eregi": [
+        "int mb_eregi(string pattern, string string [, array registers])",
+        "Case-insensitive regular expression match for multibyte string"
+    ],
+    "mb_eregi_replace": [
+        "string mb_eregi_replace(string pattern, string replacement, string string)",
+        "Case insensitive replace regular expression for multibyte string"
+    ],
+    "mb_get_info": [
+        "mixed mb_get_info([string type])",
+        "Returns the current settings of mbstring"
+    ],
+    "mb_http_input": [
+        "mixed mb_http_input([string type])",
+        "Returns the input encoding"
+    ],
+    "mb_http_output": [
+        "string mb_http_output([string encoding])",
+        "Sets the current output_encoding or returns the current output_encoding as a string"
+    ],
+    "mb_internal_encoding": [
+        "string mb_internal_encoding([string encoding])",
+        "Sets the current internal encoding or Returns the current internal encoding as a string"
+    ],
+    "mb_language": [
+        "string mb_language([string language])",
+        "Sets the current language or Returns the current language as a string"
+    ],
+    "mb_list_encodings": [
+        "mixed mb_list_encodings()",
+        "Returns an array of all supported entity encodings"
+    ],
+    "mb_output_handler": [
+        "string mb_output_handler(string contents, int status)",
+        "Returns string in output buffer converted to the http_output encoding"
+    ],
+    "mb_parse_str": [
+        "bool mb_parse_str(string encoded_string [, array result])",
+        "Parses GET/POST/COOKIE data and sets global variables"
+    ],
+    "mb_preferred_mime_name": [
+        "string mb_preferred_mime_name(string encoding)",
+        "Return the preferred MIME name (charset) as a string"
+    ],
+    "mb_regex_encoding": [
+        "string mb_regex_encoding([string encoding])",
+        "Returns the current encoding for regex as a string."
+    ],
+    "mb_regex_set_options": [
+        "string mb_regex_set_options([string options])",
+        "Set or get the default options for mbregex functions"
+    ],
+    "mb_send_mail": [
+        "int mb_send_mail(string to, string subject, string message [, string additional_headers [, string additional_parameters]])",
+        "*  Sends an email message with MIME scheme"
+    ],
+    "mb_split": [
+        "array mb_split(string pattern, string string [, int limit])",
+        "split multibyte string into array by regular expression"
+    ],
+    "mb_strcut": [
+        "string mb_strcut(string str, int start [, int length [, string encoding]])",
+        "Returns part of a string"
+    ],
+    "mb_strimwidth": [
+        "string mb_strimwidth(string str, int start, int width [, string trimmarker [, string encoding]])",
+        "Trim the string in terminal width"
+    ],
+    "mb_stripos": [
+        "int mb_stripos(string haystack, string needle [, int offset [, string encoding]])",
+        "Finds position of first occurrence of a string within another, case insensitive"
+    ],
+    "mb_stristr": [
+        "string mb_stristr(string haystack, string needle[, bool part[, string encoding]])",
+        "Finds first occurrence of a string within another, case insensitive"
+    ],
+    "mb_strlen": [
+        "int mb_strlen(string str [, string encoding])",
+        "Get character numbers of a string"
+    ],
+    "mb_strpos": [
+        "int mb_strpos(string haystack, string needle [, int offset [, string encoding]])",
+        "Find position of first occurrence of a string within another"
+    ],
+    "mb_strrchr": [
+        "string mb_strrchr(string haystack, string needle[, bool part[, string encoding]])",
+        "Finds the last occurrence of a character in a string within another"
+    ],
+    "mb_strrichr": [
+        "string mb_strrichr(string haystack, string needle[, bool part[, string encoding]])",
+        "Finds the last occurrence of a character in a string within another, case insensitive"
+    ],
+    "mb_strripos": [
+        "int mb_strripos(string haystack, string needle [, int offset [, string encoding]])",
+        "Finds position of last occurrence of a string within another, case insensitive"
+    ],
+    "mb_strrpos": [
+        "int mb_strrpos(string haystack, string needle [, int offset [, string encoding]])",
+        "Find position of last occurrence of a string within another"
+    ],
+    "mb_strstr": [
+        "string mb_strstr(string haystack, string needle[, bool part[, string encoding]])",
+        "Finds first occurrence of a string within another"
+    ],
+    "mb_strtolower": [
+        "string mb_strtolower(string sourcestring [, string encoding])",
+        "*  Returns a lowercased version of sourcestring"
+    ],
+    "mb_strtoupper": [
+        "string mb_strtoupper(string sourcestring [, string encoding])",
+        "*  Returns a uppercased version of sourcestring"
+    ],
+    "mb_strwidth": [
+        "int mb_strwidth(string str [, string encoding])",
+        "Gets terminal width of a string"
+    ],
+    "mb_substitute_character": [
+        "mixed mb_substitute_character([mixed substchar])",
+        "Sets the current substitute_character or returns the current substitute_character"
+    ],
+    "mb_substr": [
+        "string mb_substr(string str, int start [, int length [, string encoding]])",
+        "Returns part of a string"
+    ],
+    "mb_substr_count": [
+        "int mb_substr_count(string haystack, string needle [, string encoding])",
+        "Count the number of substring occurrences"
+    ],
+    "mcrypt_cbc": [
+        "string mcrypt_cbc(int cipher, string key, string data, int mode, string iv)",
+        "CBC crypt/decrypt data using key key with cipher cipher starting with iv"
+    ],
+    "mcrypt_cfb": [
+        "string mcrypt_cfb(int cipher, string key, string data, int mode, string iv)",
+        "CFB crypt/decrypt data using key key with cipher cipher starting with iv"
+    ],
+    "mcrypt_create_iv": [
+        "string mcrypt_create_iv(int size, int source)",
+        "Create an initialization vector (IV)"
+    ],
+    "mcrypt_decrypt": [
+        "string mcrypt_decrypt(string cipher, string key, string data, string mode, string iv)",
+        "OFB crypt/decrypt data using key key with cipher cipher starting with iv"
+    ],
+    "mcrypt_ecb": [
+        "string mcrypt_ecb(int cipher, string key, string data, int mode, string iv)",
+        "ECB crypt/decrypt data using key key with cipher cipher starting with iv"
+    ],
+    "mcrypt_enc_get_algorithms_name": [
+        "string mcrypt_enc_get_algorithms_name(resource td)",
+        "Returns the name of the algorithm specified by the descriptor td"
+    ],
+    "mcrypt_enc_get_block_size": [
+        "int mcrypt_enc_get_block_size(resource td)",
+        "Returns the block s
