b9113e9fe4cfb78ae6282e3a0f832e7f2fa62527
Binatray <107320988+Binatray@users.noreply.github.com>
2021-11-28 19:36:38 +0000

    Added lodash

--- src/main/resources/static/js/ace/worker-xquery.js
+++ src/main/resources/static/js/ace/worker-xquery.js
@@ -1285,4 +1285,384 @@ exports.deferredCall = function(fcn) {
 
     var deferred = function(timeout) {
         deferred.cancel();
-        timer = setTimeout(callback, timeout 
+        timer = setTimeout(callback, timeout || 0);
+        return deferred;
+    };
+
+    deferred.schedule = deferred;
+
+    deferred.call = function() {
+        this.cancel();
+        fcn();
+        return deferred;
+    };
+
+    deferred.cancel = function() {
+        clearTimeout(timer);
+        timer = null;
+        return deferred;
+    };
+    
+    deferred.isPending = function() {
+        return timer;
+    };
+
+    return deferred;
+};
+
+
+exports.delayedCall = function(fcn, defaultTimeout) {
+    var timer = null;
+    var callback = function() {
+        timer = null;
+        fcn();
+    };
+
+    var _self = function(timeout) {
+        if (timer == null)
+            timer = setTimeout(callback, timeout || defaultTimeout);
+    };
+
+    _self.delay = function(timeout) {
+        timer && clearTimeout(timer);
+        timer = setTimeout(callback, timeout || defaultTimeout);
+    };
+    _self.schedule = _self;
+
+    _self.call = function() {
+        this.cancel();
+        fcn();
+    };
+
+    _self.cancel = function() {
+        timer && clearTimeout(timer);
+        timer = null;
+    };
+
+    _self.isPending = function() {
+        return timer;
+    };
+
+    return _self;
+};
+});
+
+define("ace/worker/mirror",[], function(require, exports, module) {
+"use strict";
+
+var Range = require("../range").Range;
+var Document = require("../document").Document;
+var lang = require("../lib/lang");
+    
+var Mirror = exports.Mirror = function(sender) {
+    this.sender = sender;
+    var doc = this.doc = new Document("");
+    
+    var deferredUpdate = this.deferredUpdate = lang.delayedCall(this.onUpdate.bind(this));
+    
+    var _self = this;
+    sender.on("change", function(e) {
+        var data = e.data;
+        if (data[0].start) {
+            doc.applyDeltas(data);
+        } else {
+            for (var i = 0; i < data.length; i += 2) {
+                if (Array.isArray(data[i+1])) {
+                    var d = {action: "insert", start: data[i], lines: data[i+1]};
+                } else {
+                    var d = {action: "remove", start: data[i], end: data[i+1]};
+                }
+                doc.applyDelta(d, true);
+            }
+        }
+        if (_self.$timeout)
+            return deferredUpdate.schedule(_self.$timeout);
+        _self.onUpdate();
+    });
+};
+
+(function() {
+    
+    this.$timeout = 500;
+    
+    this.setTimeout = function(timeout) {
+        this.$timeout = timeout;
+    };
+    
+    this.setValue = function(value) {
+        this.doc.setValue(value);
+        this.deferredUpdate.schedule(this.$timeout);
+    };
+    
+    this.getValue = function(callbackId) {
+        this.sender.callback(this.doc.getValue(), callbackId);
+    };
+    
+    this.onUpdate = function() {
+    };
+    
+    this.isPending = function() {
+        return this.deferredUpdate.isPending();
+    };
+    
+}).call(Mirror.prototype);
+
+});
+
+define("ace/mode/xquery/xqlint",[], function(require, exports, module) {
+module.exports = (function outer (modules, cache, entry) {
+    var previousRequire = typeof require == "function" && require;
+    function newRequire(name, jumped){
+        if(!cache[name]) {
+            if(!modules[name]) {
+                var currentRequire = typeof require == "function" && require;
+                if (!jumped && currentRequire) return currentRequire(name, true);
+                if (previousRequire) return previousRequire(name, true);
+                var err = new Error('Cannot find module \'' + name + '\'');
+                err.code = 'MODULE_NOT_FOUND';
+                throw err;
+            }
+            var m = cache[name] = {exports:{}};
+            modules[name][0].call(m.exports, function(x){
+                var id = modules[name][1][x];
+                return newRequire(id ? id : x);
+            },m,m.exports,outer,modules,cache,entry);
+        }
+        return cache[name].exports;
+    }
+    for(var i=0;i<entry.length;i++) newRequire(entry[i]);
+    return newRequire(entry[0]);
+})
+({"/node_modules/xqlint/lib/compiler/errors.js":[function(_dereq_,module,exports){
+'use strict';
+
+var init = function(that, code, message, pos, type){
+    if(!code) {
+        throw new Error(type + ' code is missing.');
+    }
+    
+    if(!message) {
+        throw new Error(type + ' message is missing.');
+    }
+    
+    if(!pos) {
+        throw new Error(type + ' position is missing.');
+    }
+
+    that.getCode = function(){
+        return code;
+    };
+    
+    that.getMessage = function(){
+        return message;
+    };
+
+    that.getPos = function(){
+        return pos;
+    };
+};
+
+var StaticError = {};
+var StaticWarning = {};
+StaticError.prototype = new Error();
+StaticWarning.prototype = new Error();
+
+exports.StaticError = StaticError.prototype.constructor = function(code, message, pos) {
+    init(this, code, message, pos, 'Error');
+};
+
+exports.StaticWarning = StaticWarning.prototype.constructor = function(code, message, pos) {
+    init(this, code, message, pos, 'Warning');
+};
+},{}],"/node_modules/xqlint/lib/compiler/handlers.js":[function(_dereq_,module,exports){
+'use strict';
+
+var TreeOps = _dereq_('../tree_ops').TreeOps;
+var Errors = _dereq_('./errors');
+var StaticWarning = Errors.StaticWarning;
+exports.ModuleDecl = function(translator, rootSctx, node){
+    var prefix = '';
+    return {
+        NCName: function(ncname){
+            prefix = TreeOps.flatten(ncname);
+        },
+
+        URILiteral: function(uri) {
+            uri = TreeOps.flatten(uri);
+            uri = uri.substring(1, uri.length - 1);
+            translator.apply(function(){
+                rootSctx.moduleNamespace = uri;
+                rootSctx.addNamespace(uri, prefix, node.pos, 'moduleDecl');
+            });
+        }
+    };
+};
+
+exports.ModuleImport = function(translator, rootSctx, node) {
+    var prefix = '';
+    var moduleURI;
+
+    return {
+        NCName: function(ncname){
+            prefix = TreeOps.flatten(ncname);
+        },
+
+        URILiteral: function(uri) {
+            if(moduleURI !== undefined) {
+                return;
+            }
+            uri = TreeOps.flatten(uri);
+            uri = uri.substring(1, uri.length - 1);
+            moduleURI = uri;
+            translator.apply(function(){
+                rootSctx.importModule(uri, prefix, node.pos);
+            });
+        }
+    };
+};
+
+exports.SchemaImport = function(translator, rootSctx, node) {
+    var prefix = '';
+    var schemaURI;
+    
+    return {
+        SchemaPrefix: function(schemaPrefix) {
+            var SchemaPrefixHandler = function () {
+                this.NCName = function (ncname) {
+                    prefix = TreeOps.flatten(ncname);
+                };
+            };
+            translator.visitChildren(schemaPrefix, new SchemaPrefixHandler());
+        },
+
+        URILiteral: function(uri) {
+            if(schemaURI !== undefined) {
+                return;
+            }
+            uri = TreeOps.flatten(uri);
+            uri = uri.substring(1, uri.length - 1);
+            schemaURI = uri;
+            translator.apply(function(){
+                rootSctx.addNamespace(uri, prefix, node.pos, 'schema');
+            });
+        }
+    };
+};
+
+exports.DefaultNamespaceDecl = function(translator, rootSctx, node) {
+    var fn = false;
+    var ns = '';
+
+    return {
+        TOKEN: function(token){
+            fn = fn ? true : (token.value === 'function');
+        },
+        URILiteral: function(uri){
+            ns = TreeOps.flatten(uri);
+            ns = ns.substring(1, ns.length - 1);
+            if(!fn) {
+                translator.apply(function(){
+                    throw new StaticWarning('W06', 'Avoid default element namespace declarations.', node.pos);
+                });
+                rootSctx.defaultElementNamespace = ns;
+            } else {
+                rootSctx.defaultFunctionNamespace = ns;
+            }
+        }
+    };
+};
+
+exports.NamespaceDecl = function(translator, rootSctx, node) {
+    var prefix = '';
+    return {
+        NCName: function(ncname) {
+            prefix = TreeOps.flatten(ncname);
+        },
+        URILiteral: function(uri) {
+            uri = TreeOps.flatten(uri);
+            uri = uri.substring(1, uri.length - 1);
+            translator.apply(function(){
+                rootSctx.addNamespace(uri, prefix, node.pos, 'declare');
+            });
+        }
+    };
+};
+exports.VarHandler = function(translator, sctx, node){
+    var EQNameHandler = function(eqname){
+        var value = TreeOps.flatten(eqname);
+        translator.apply(function(){
+            var qname = sctx.resolveQName(value, eqname.pos);
+            sctx.addVariable(qname, node.name, eqname.pos);
+        });
+    };
+    return {
+        ExprSingle: function(){ return true; },
+        VarValue: function(){ return true; },
+        VarDefaultValue: function(){ return true; },
+        VarName: EQNameHandler,
+        EQName: EQNameHandler
+    };
+};
+
+exports.VarRefHandler = function(translator, sctx, node){
+    return {
+        VarName: function(eqname){
+            var value = TreeOps.flatten(eqname);
+            translator.apply(function(){
+                var qname = sctx.resolveQName(value, node.pos);
+                if(qname.uri !== '') {
+                    sctx.root.namespaces[qname.uri].used = true;
+                }
+                sctx.addVarRef(qname, eqname.pos);
+            });
+        }
+    };
+};
+},{"../tree_ops":"/node_modules/xqlint/lib/tree_ops.js","./errors":"/node_modules/xqlint/lib/compiler/errors.js"}],"/node_modules/xqlint/lib/compiler/schema_built-in_types.js":[function(_dereq_,module,exports){
+'use strict';
+exports.getSchemaBuiltinTypes = function(){
+    var ns = 'http://www.w3.org/2001/XMLSchema';
+    var SchemaBuiltinTypes = {};
+    SchemaBuiltinTypes[ns] = {
+        variables: {},
+        functions: {}
+    };
+    SchemaBuiltinTypes[ns].functions[ns + '#string#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'string', arity: 1, eqname: { uri: ns, name: 'string' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#boolean#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'boolean', arity: 1, eqname: { uri: ns, name: 'boolean' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#decimal#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'decimal', arity: 1, eqname: { uri: ns, name: 'decimal' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#float#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'float', arity: 1, eqname: { uri: ns, name: 'float' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#double#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'double', arity: 1, eqname: { uri: ns, name: 'double' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#duration#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'duration', arity: 1, eqname: { uri: ns, name: 'duration' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#dateTime#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'dateTime', arity: 1, eqname: { uri: ns, name: 'dateTime' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#time#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'time', arity: 1, eqname: { uri: ns, name: 'time' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#date#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'date', arity: 1, eqname: { uri: ns, name: 'date' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#gYearMonth#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'gYearMonth', arity: 1, eqname: { uri: ns, name: 'gYearMonth' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#gYear#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'gYear', arity: 1, eqname: { uri: ns, name: 'gYear' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#gMonthDay#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'gMonthDay', arity: 1, eqname: { uri: ns, name: 'gMonthDay' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#gDay#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'gDay', arity: 1, eqname: { uri: ns, name: 'gDay' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#gMonth#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'gMonth', arity: 1, eqname: { uri: ns, name: 'gMonth' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#hexBinary#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'hexBinary', arity: 1, eqname: { uri: ns, name: 'hexBinary' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#base64Binary#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'base64Binary', arity: 1, eqname: { uri: ns, name: 'base64Binary' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#anyURI#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'anyURI', arity: 1, eqname: { uri: ns, name: 'anyURI' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#QName#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'QName', arity: 1, eqname: { uri: ns, name: 'QName' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#normalizedString#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'normalizedString', arity: 1, eqname: { uri: ns, name: 'normalizedString' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#token#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'token', arity: 1, eqname: { uri: ns, name: 'token' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#language#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'language', arity: 1, eqname: { uri: ns, name: 'language' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#NMTOKEN#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'NMTOKEN', arity: 1, eqname: { uri: ns, name: 'NMTOKEN' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#Name#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'Name', arity: 1, eqname: { uri: ns, name: 'Name' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#NCName#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'NCName', arity: 1, eqname: { uri: ns, name: 'NCName' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#ID#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'ID', arity: 1, eqname: { uri: ns, name: 'ID' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#IDREF#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'IDREF', arity: 1, eqname: { uri: ns, name: 'IDREF' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#ENTITY#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'ENTITY', arity: 1, eqname: { uri: ns, name: 'ENTITY' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#integer#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'integer', arity: 1, eqname: { uri: ns, name: 'integer' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#nonPositiveInteger#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'nonPositiveInteger', arity: 1, eqname: { uri: ns, name: 'nonPositiveInteger' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#negativeInteger#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'negativeInteger', arity: 1, eqname: { uri: ns, name: 'negativeInteger' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#long#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'long', arity: 1, eqname: { uri: ns, name: 'long' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#int#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'int', arity: 1, eqname: { uri: ns, name: 'int' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#short#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'short', arity: 1, eqname: { uri: ns, name: 'short' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#byte#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'byte', arity: 1, eqname: { uri: ns, name: 'byte' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#nonNegativeInteger#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'nonNegativeInteger', arity: 1, eqname: { uri: ns, name: 'nonNegativeInteger' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#unsignedLong#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'unsignedLong', arity: 1, eqname: { uri: ns, name: 'unsignedLong' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#unsignedInt#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'unsignedInt', arity: 1, eqname: { uri: ns, name: 'unsignedInt' } };
+    SchemaBuiltinTypes[ns].functions[ns + '#unsignedShort#1'] = { params: ['$arg as xs:anyAtomicType?'], annotations: [], name: 'unsignedShort', arity: 1, eqname: { uri: ns, name: 'unsignedShort' } };
+    SchemaBuiltinTypes[ns].functi
