3a6cac972f10c574d5507c35d9fa83457fbd8064
Binatray <107320988+Binatray@users.noreply.github.com>
2022-02-18 16:52:13 +0000

    fixed class name

--- src/main/resources/static/js/ace/worker-xquery.js
+++ src/main/resources/static/js/ace/worker-xquery.js
@@ -6679,4 +6679,332 @@ XQueryTokenizer.TOKEN =
 var JSONiqTokenizer = _dereq_('./JSONiqTokenizer').JSONiqTokenizer;
 var Lexer = _dereq_('./lexer').Lexer;
 
-var keys = 'NaN|after|allowing|ancestor|ancestor-or-self|and|append|array|as|ascending|at|attribute|base-uri|before|boundary-space|break|by|case|cast|castable|catch|child|collation|comment|constraint|construction|contains|context|continue|copy|copy-namespaces|count|decimal-format|decimal-separator|declare|default|delete|descendant|descendant-or-self|descending|digit|div|document|document-node|element|else|empty|empty-sequence|encoding|end|eq|every|except|exit|external|false|first|following|following-sibling|for|from|ft-option|function|ge|greatest|group|grouping-separator|gt|idiv|if|import|in|index|infinity|insert|instance|integrity|intersect|into|is|item|json|json-item|jsoniq|last|lax|le|least|let|loop|lt|minus-sign|mod|modify|module|namespace|namespace-node|ne|next|node|nodes|not|null|object|of|only|option|or|order|ordered|ordering|paragraphs|parent|pattern-separator|per-mille|percent|preceding|preceding-sibling|previous|processing-instruction|rename|replace|return|returning|revalidation|satisfies|schema|schema-attribute|schema-element|score|select|self|sentences|sliding|some|stable|start|strict|switch|text|then|times|to|treat|true|try|tumbling|type|typeswitch|union|unordered
+var keys = 'NaN|after|allowing|ancestor|ancestor-or-self|and|append|array|as|ascending|at|attribute|base-uri|before|boundary-space|break|by|case|cast|castable|catch|child|collation|comment|constraint|construction|contains|context|continue|copy|copy-namespaces|count|decimal-format|decimal-separator|declare|default|delete|descendant|descendant-or-self|descending|digit|div|document|document-node|element|else|empty|empty-sequence|encoding|end|eq|every|except|exit|external|false|first|following|following-sibling|for|from|ft-option|function|ge|greatest|group|grouping-separator|gt|idiv|if|import|in|index|infinity|insert|instance|integrity|intersect|into|is|item|json|json-item|jsoniq|last|lax|le|least|let|loop|lt|minus-sign|mod|modify|module|namespace|namespace-node|ne|next|node|nodes|not|null|object|of|only|option|or|order|ordered|ordering|paragraphs|parent|pattern-separator|per-mille|percent|preceding|preceding-sibling|previous|processing-instruction|rename|replace|return|returning|revalidation|satisfies|schema|schema-attribute|schema-element|score|select|self|sentences|sliding|some|stable|start|strict|switch|text|then|times|to|treat|true|try|tumbling|type|typeswitch|union|unordered|updating|validate|value|variable|version|when|where|while|window|with|words|xquery|zero-digit'.split('|');
+var keywords = keys.map(function(val) { return { name: '\'' + val + '\'', token: 'keyword' }; });
+var ncnames = keys.map(function(val) { return { name: '\'' + val + '\'', token: 'text', next: function(stack){ stack.pop(); } }; });
+
+var cdata = 'constant.language';
+var number = 'constant';
+var xmlcomment = 'comment';
+var pi = 'xml-pe';
+var pragma = 'constant.buildin';
+var n = function(name){
+    return '\'' + name + '\'';
+};
+var Rules = {
+    start: [
+        { name: n('(#'), token: pragma, next: function(stack){ stack.push('Pragma'); } },
+        { name: n('(:'), token: 'comment', next: function(stack){ stack.push('Comment'); } },
+        { name: n('(:~'), token: 'comment.doc', next: function(stack){ stack.push('CommentDoc'); } },
+        { name: n('<!--'), token: xmlcomment, next: function(stack){ stack.push('XMLComment'); } },
+        { name: n('<?'), token: pi, next: function(stack) { stack.push('PI'); } },
+        { name: n('\'\''), token: 'string', next: function(stack){ stack.push('AposString'); } },
+        { name: n('"'), token: 'string', next: function(stack){ stack.push('QuotString'); } },
+        { name: 'Annotation', token: 'support.function' },
+        { name: 'ModuleDecl', token: 'keyword', next: function(stack){ stack.push('Prefix'); } },
+        { name: 'OptionDecl', token: 'keyword', next: function(stack){ stack.push('_EQName'); } },
+        { name: 'AttrTest', token: 'support.type' },
+        { name: 'Variable', token: 'variable' },
+        { name: n('<![CDATA['), token: cdata, next: function(stack){ stack.push('CData'); } },
+        { name: 'IntegerLiteral', token: number },
+        { name: 'DecimalLiteral', token: number },
+        { name: 'DoubleLiteral', token: number },
+        { name: 'Operator', token: 'keyword.operator' },
+        { name: 'EQName', token: function(val) { return keys.indexOf(val) !== -1 ? 'keyword' : 'support.function'; } },
+        { name: n('('), token: 'lparen' },
+        { name: n(')'), token: 'rparen' },
+        { name: 'Tag', token: 'meta.tag', next: function(stack){ stack.push('StartTag'); } },
+        { name: n('}'), token: 'text', next: function(stack){ if(stack.length > 1) { stack.pop(); } } },
+        { name: n('{'), token: 'text', next: function(stack){ stack.push('start'); } } //, next: function(stack){ if(stack.length > 1) { stack.pop(); } } }
+    ].concat(keywords),
+    _EQName: [
+        { name: 'EQName', token: 'text', next: function(stack) { stack.pop(); } }
+    ].concat(ncnames),
+    Prefix: [
+        { name: 'NCName', token: 'text', next: function(stack) { stack.pop(); } }
+    ].concat(ncnames),
+    StartTag: [
+        { name: n('>'), token: 'meta.tag', next: function(stack){ stack.push('TagContent'); } },
+        { name: 'QName', token: 'entity.other.attribute-name' },
+        { name: n('='), token: 'text' },
+        { name: n('\'\''), token: 'string', next: function(stack){ stack.push('AposAttr'); } },
+        { name: n('"'), token: 'string', next: function(stack){ stack.push('QuotAttr'); } },
+        { name: n('/>'), token: 'meta.tag.r', next: function(stack){ stack.pop(); } }
+    ],
+    TagContent: [
+        { name: 'ElementContentChar', token: 'text' },
+        { name: n('<![CDATA['), token: cdata, next: function(stack){ stack.push('CData'); } },
+        { name: n('<!--'), token: xmlcomment, next: function(stack){ stack.push('XMLComment'); } },
+        { name: 'Tag', token: 'meta.tag', next: function(stack){ stack.push('StartTag'); } },
+        { name: 'PredefinedEntityRef', token: 'constant.language.escape' },
+        { name: 'CharRef', token: 'constant.language.escape' },
+        { name: n('{{'), token: 'text' },
+        { name: n('}}'), token: 'text' },
+        { name: n('{'), token: 'text', next: function(stack){ stack.push('start'); } },
+        { name: 'EndTag', token: 'meta.tag', next: function(stack){ stack.pop(); stack.pop(); } }
+    ],
+    AposAttr: [
+        { name: n('\'\''), token: 'string', next: function(stack){ stack.pop(); } },
+        { name: 'EscapeApos', token: 'constant.language.escape' },
+        { name: 'AposAttrContentChar', token: 'string' },
+        { name: 'PredefinedEntityRef', token: 'constant.language.escape' },
+        { name: 'CharRef', token: 'constant.language.escape' },
+        { name: n('{{'), token: 'string' },
+        { name: n('}}'), token: 'string' },
+        { name: n('{'), token: 'text', next: function(stack){ stack.push('start'); } }
+    ],
+    QuotAttr: [
+        { name: n('\"'), token: 'string', next: function(stack){ stack.pop(); } },
+        { name: 'EscapeQuot', token: 'constant.language.escape' },
+        { name: 'QuotAttrContentChar', token: 'string' },
+        { name: 'PredefinedEntityRef', token: 'constant.language.escape' },
+        { name: 'CharRef', token: 'constant.language.escape' },
+        { name: n('{{'), token: 'string' },
+        { name: n('}}'), token: 'string' },
+        { name: n('{'), token: 'text', next: function(stack){ stack.push('start'); } }
+    ],
+    Pragma: [
+        { name: 'PragmaContents', token: pragma },
+        { name: n('#'), token: pragma },
+        { name: n('#)'), token: pragma, next: function(stack){ stack.pop(); } }
+    ],
+    Comment: [
+        { name: 'CommentContents', token: 'comment' },
+        { name: n('(:'), token: 'comment', next: function(stack){ stack.push('Comment'); } },
+        { name: n(':)'), token: 'comment', next: function(stack){ stack.pop(); } }
+    ],
+    CommentDoc: [
+        { name: 'DocCommentContents', token: 'comment.doc' },
+        { name: 'DocTag', token: 'comment.doc.tag' },
+        { name: n('(:'), token: 'comment.doc', next: function(stack){ stack.push('CommentDoc'); } },
+        { name: n(':)'), token: 'comment.doc', next: function(stack){ stack.pop(); } }
+    ],
+    XMLComment: [
+        { name: 'DirCommentContents', token: xmlcomment },
+        { name: n('-->'), token: xmlcomment, next: function(stack){ stack.pop(); } }
+    ],
+    CData: [
+        { name: 'CDataSectionContents', token: cdata },
+        { name: n(']]>'), token: cdata, next: function(stack){ stack.pop(); } }
+    ],
+    PI: [
+        { name: 'DirPIContents', token: pi },
+        { name: n('?'), token: pi },
+        { name: n('?>'), token: pi, next: function(stack){ stack.pop(); } }
+    ],
+    AposString: [
+        { name: n('\'\''), token: 'string', next: function(stack){ stack.pop(); } },
+        { name: 'PredefinedEntityRef', token: 'constant.language.escape' },
+        { name: 'CharRef', token: 'constant.language.escape' },
+        { name: 'EscapeApos', token: 'constant.language.escape' },
+        { name: 'AposChar', token: 'string' }
+    ],
+    QuotString: [
+        { name: n('"'), token: 'string', next: function(stack){ stack.pop(); } },
+        { name: 'JSONPredefinedCharRef', token: 'constant.language.escape' },
+        { name: 'JSONCharRef', token: 'constant.language.escape' },
+        { name: 'JSONChar', token: 'string' }
+    ]
+};
+    
+exports.JSONiqLexer = function(){ return new Lexer(JSONiqTokenizer, Rules); };
+},{"./JSONiqTokenizer":"/node_modules/xqlint/lib/lexers/JSONiqTokenizer.js","./lexer":"/node_modules/xqlint/lib/lexers/lexer.js"}],"/node_modules/xqlint/lib/lexers/lexer.js":[function(_dereq_,module,exports){
+'use strict';
+
+var TokenHandler = function(code) {
+    var input = code;
+    this.tokens = [];
+ 
+    this.reset = function() {
+        input = input;
+        this.tokens = [];
+    };
+    
+    this.startNonterminal = function() {};
+    this.endNonterminal = function() {};
+
+    this.terminal = function(name, begin, end) {
+        this.tokens.push({
+            name: name,
+            value: input.substring(begin, end)
+        });
+    };
+
+    this.whitespace = function(begin, end) {
+        this.tokens.push({
+            name: 'WS',
+            value: input.substring(begin, end)
+        });
+    };
+};
+
+exports.Lexer = function(Tokenizer, Rules) {
+
+    this.tokens = [];
+  
+    this.getLineTokens = function(line, state) {
+        state = (state === 'start' || !state) ? '["start"]' : state;
+        var stack = JSON.parse(state);
+        var h = new TokenHandler(line);
+        var tokenizer = new Tokenizer(line, h);
+        var tokens = [];
+    
+        while(true) {
+            var currentState = stack[stack.length - 1];
+            try {
+                h.tokens = [];
+                tokenizer['parse_' + currentState]();
+                var info = null;
+        
+                if(h.tokens.length > 1 && h.tokens[0].name === 'WS') {
+                    tokens.push({
+                        type: 'text',
+                        value: h.tokens[0].value
+                    });
+                    h.tokens.splice(0, 1);
+                }
+        
+                var token = h.tokens[0];
+                var rules  = Rules[currentState];
+                for(var k = 0; k < rules.length; k++) {
+                    var rule = Rules[currentState][k];
+                    if((typeof(rule.name) === 'function' && rule.name(token)) || rule.name === token.name) {
+                        info = rule;
+                        break;
+                    }
+                }
+        
+                if(token.name === 'EOF') { break; }
+                if(token.value === '') { throw 'Encountered empty string lexical rule.'; }
+        
+                tokens.push({
+                    type: info === null ? 'text' : (typeof(info.token) === 'function' ? info.token(token.value) : info.token),
+                    value: token.value
+                });
+        
+                if(info && info.next) {
+                    info.next(stack);
+                }
+      
+            } catch(e) {
+                if(e instanceof tokenizer.ParseException) {
+                    var index = 0;
+                    for(var i=0; i < tokens.length; i++) {
+                        index += tokens[i].value.length;
+                    }
+                    tokens.push({ type: 'text', value: line.substring(index) });
+                    return {
+                        tokens: tokens,
+                        state: JSON.stringify(['start'])
+                    };
+                } else {
+                    throw e;
+                }
+            }
+        }
+
+        return {
+            tokens: tokens,
+            state: JSON.stringify(stack)
+        };
+    };
+};
+},{}],"/node_modules/xqlint/lib/lexers/xquery_lexer.js":[function(_dereq_,module,exports){
+'use strict';
+
+var XQueryTokenizer = _dereq_('./XQueryTokenizer').XQueryTokenizer;
+var Lexer = _dereq_('./lexer').Lexer;
+
+var keys = 'after|ancestor|ancestor-or-self|and|as|ascending|attribute|before|case|cast|castable|child|collation|comment|copy|count|declare|default|delete|descendant|descendant-or-self|descending|div|document|document-node|element|else|empty|empty-sequence|end|eq|every|except|first|following|following-sibling|for|function|ge|group|gt|idiv|if|import|insert|instance|intersect|into|is|item|last|le|let|lt|mod|modify|module|namespace|namespace-node|ne|node|only|or|order|ordered|parent|preceding|preceding-sibling|processing-instruction|rename|replace|return|satisfies|schema-attribute|schema-element|self|some|stable|start|switch|text|to|treat|try|typeswitch|union|unordered|validate|where|with|xquery|contains|paragraphs|sentences|times|words|by|collectionreturn|variable|version|option|when|encoding|toswitch|catch|tumbling|sliding|window|at|using|stemming|collection|schema|while|on|nodes|index|external|then|in|updating|value|of|containsbreak|loop|continue|exit|returning|append|json|position|strict'.split('|');
+
+var keywords = keys.map(function(val) { return { name: '\'' + val + '\'', token: 'keyword' }; });
+var ncnames = keys.map(function(val) { return { name: '\'' + val + '\'', token: 'text', next: function(stack){ stack.pop(); } }; });
+
+var cdata = 'constant.language';
+var number = 'constant';
+var xmlcomment = 'comment';
+var pi = 'xml-pe';
+var pragma = 'constant.buildin';
+var n = function(name){
+    return '\'' + name + '\'';
+};
+var Rules = {
+    start: [
+        { name: n('(#'), token: pragma, next: function(stack){ stack.push('Pragma'); } },
+        { name: n('(:'), token: 'comment', next: function(stack){ stack.push('Comment'); } },
+        { name: n('(:~'), token: 'comment.doc', next: function(stack){ stack.push('CommentDoc'); } },
+        { name: n('<!--'), token: xmlcomment, next: function(stack){ stack.push('XMLComment'); } },
+        { name: n('<?'), token: pi, next: function(stack) { stack.push('PI'); } },
+        { name: n('\'\''), token: 'string', next: function(stack){ stack.push('AposString'); } },
+        { name: n('"'), token: 'string', next: function(stack){ stack.push('QuotString'); } },
+        { name: 'Annotation', token: 'support.function' },
+        { name: 'ModuleDecl', token: 'keyword', next: function(stack){ stack.push('Prefix'); } },
+        { name: 'OptionDecl', token: 'keyword', next: function(stack){ stack.push('_EQName'); } },
+        { name: 'AttrTest', token: 'support.type' },
+        { name: 'Variable', token: 'variable' },
+        { name: n('<![CDATA['), token: cdata, next: function(stack){ stack.push('CData'); } },
+        { name: 'IntegerLiteral', token: number },
+        { name: 'DecimalLiteral', token: number },
+        { name: 'DoubleLiteral', token: number },
+        { name: 'Operator', token: 'keyword.operator' },
+        { name: 'EQName', token: function(val) { return keys.indexOf(val) !== -1 ? 'keyword' : 'support.function'; } },
+        { name: n('('), token: 'lparen' },
+        { name: n(')'), token: 'rparen' },
+        { name: 'Tag', token: 'meta.tag', next: function(stack){ stack.push('StartTag'); } },
+        { name: n('}'), token: 'text', next: function(stack){ if(stack.length > 1) { stack.pop(); } } },
+        { name: n('{'), token: 'text', next: function(stack){ stack.push('start'); } } //, next: function(stack){ if(stack.length > 1) { stack.pop(); } } }
+    ].concat(keywords),
+    _EQName: [
+        { name: 'EQName', token: 'text', next: function(stack) { stack.pop(); } }
+    ].concat(ncnames),
+    Prefix: [
+        { name: 'NCName', token: 'text', next: function(stack) { stack.pop(); } }
+    ].concat(ncnames),
+    StartTag: [
+        { name: n('>'), token: 'meta.tag', next: function(stack){ stack.push('TagContent'); } },
+        { name: 'QName', token: 'entity.other.attribute-name' },
+        { name: n('='), token: 'text' },
+        { name: n('\'\''), token: 'string', next: function(stack){ stack.push('AposAttr'); } },
+        { name: n('"'), token: 'string', next: function(stack){ stack.push('QuotAttr'); } },
+        { name: n('/>'), token: 'meta.tag.r', next: function(stack){ stack.pop(); } }
+    ],
+    TagContent: [
+        { name: 'ElementContentChar', token: 'text' },
+        { name: n('<![CDATA['), token: cdata, next: function(stack){ stack.push('CData'); } },
+        { name: n('<!--'), token: xmlcomment, next: function(stack){ stack.push('XMLComment'); } },
+        { name: 'Tag', token: 'meta.tag', next: function(stack){ stack.push('StartTag'); } },
+        { name: 'PredefinedEntityRef', token: 'constant.language.escape' },
+        { name: 'CharRef', token: 'constant.language.escape' },
+        { name: n('{{'), token: 'text' },
+        { name: n('}}'), token: 'text' },
+        { name: n('{'), token: 'text', next: function(stack){ stack.push('start'); } },
+        { name: 'EndTag', token: 'meta.tag', next: function(stack){ stack.pop(); stack.pop(); } }
+    ],
+    AposAttr: [
+        { name: n('\'\''), token: 'string', next: function(stack){ stack.pop(); } },
+        { name: 'EscapeApos', token: 'constant.language.escape' },
+        { name: 'AposAttrContentChar', token: 'string' },
+        { name: 'PredefinedEntityRef', token: 'constant.language.escape' },
+        { name: 'CharRef', token: 'constant.language.escape' },
+        { name: n('{{'), token: 'string' },
+        { name: n('}}'), token: 'string' },
+        { name: n('{'), token: 'text', next: function(stack){ stack.push('start'); } }
+    ],
+    QuotAttr: [
+        { name: n('\"'), token: 'string', next: function(stack){ stack.pop(); } },
+        { name: 'EscapeQuot', token: 'constant.language.escape' },
+        { name: 'QuotAttrContentChar', token: 'string' },
+        { name: 'PredefinedEntityRef', token: 'constant.language.escape' },
+        { name: 'CharRef', token: 'constant.language.escape' },
+        { name: n('{{'), token: 'string' },
+        { name: n('}}'), token: 'string' },
+        { name: n('{'), token: 'text', next: function(stack){ stack.push('start'); } }
+    ],
+    Pragma: [
+        { name: 'PragmaContents', token: pragma },
+        { name: n('#'), token: pragma },
+        { name: n('#)'), token: pragma, next: function(stack){ stack.pop(); } }
+    ],
+    Comment: [
+        { name: 'CommentContents', token: 'comment' },
+        { name: n('(:'), token: 
